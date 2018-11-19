ded20f7b51315763c07cde531d3216d7b421dd3e
Angel Barranco <angelusualle@gmail.com>
2018-11-19 17:21:11 -0500

    Added DB

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -12,19 +12,43 @@ export class Home extends Component {
     isLoading: false,
     invalid: false,
     successMessage: '',
-    isSubbed: false
+    isSubbed: false,
+    failureMessage: '',
+    isFailure: false
+  }
+
+  initState={
+    URL: '',
+    Email: '',
+    MobileNumber: '',
+    XPath: '',
+    isLoading: false,
+    invalid: false,
+    successMessage: '',
+    isSubbed: false,
+    failureMessage: '',
+    isFailure: false
+  }
+  
+  reset(){
+    this.setState(this.state.initState);
+  }
+
+  validData(){
+    if (this.state.URL.length === 0 || this.state.Email.length === 0) return false;
+    return true;
   }
 
   handleChange(id, val) {
     this.setState({[id]:val})
-    if (this.state.URL.length !== 0 || this.state.Email.length !== 0) {
+    if (!this.validData()) {
       this.setState({invalid:false})
       return;
     }
   }
 
   handleSub(){
-    if (this.state.URL.length === 0 || this.state.Email.length === 0) {
+    if (!this.validData()) {
       this.setState({invalid:true})
       return;
     }
@@ -34,9 +58,10 @@ export class Home extends Component {
           body: JSON.stringify(this.state),
           headers:{'Content-Type': 'application/json;charset=UTF-8'}
         })
-    .then(response => {
-        this.setState({successMessage: response, isSubbed: true});
-    }).catch((e) => console.log(e));
+        .then((response) => {
+          response.text().then((t) => this.setState({successMessage: t, isSubbed: true, isLoading: false}));
+        })
+        .catch((e) => this.setState({failureMessage: e.message, isFailure: true}));
   }
 
   render() {
@@ -59,6 +84,7 @@ export class Home extends Component {
             type="text"
             label="*URL"
             onChange={(e) => this.handleChange(e.target.id, e.target.value)}
+            
             placeholder="http://somesitetowatch.com"
           />
           <FieldGroup
@@ -66,6 +92,7 @@ export class Home extends Component {
             type="email"
             label="*Email"
             onChange={(e) => this.handleChange(e.target.id, e.target.value)}
+            disabled={this.state.isSubbed}
             placeholder="YourEmail@domain.com"
           />
           <FieldGroup
@@ -73,6 +100,7 @@ export class Home extends Component {
             type="tel"
             label="Mobile Phone"
             onChange={(e) => this.handleChange(e.target.id, e.target.value)}
+            disabled={this.state.isSubbed}
             placeholder="15555555"
           />
           <FieldGroup
@@ -81,15 +109,31 @@ export class Home extends Component {
             label="XPath"
             onChange={(e) => this.handleChange(e.target.id, e.target.value)}
             placeholder="(//table[contains(@summary, 'This layout table is used to present the seating numbers.')]//tr)[position() < last()]"
+            disabled={this.state.isSubbed}
           />
           <p>* Required field.</p>
-          <Button bsStyle="primary" bsSize="large" disabled={this.state.isLoading} onClick={!this.state.isLoading ? (e) => this.handleSub(): null}>
-            {this.state.isLoading ? 'Subscribing..' : "Subscribe!"}
+          <Button bsStyle="primary" bsSize="large" disabled={this.state.isLoading || this.state.isSubbed} onClick={!this.state.isLoading ? (e) => this.handleSub(): null}>
+            {this.state.isLoading && 'Subscribing..'}
+            {this.state.isSubbed && 'Subscribed!'}
+            {!this.state.isSubbed && !this.state.isLoading && 'Subscribe!'}
           </Button>
+          {this.state.isSubbed &&
+            <Button bsStyle="info" bsSize="large" onClick={(e) => this.reset()}>
+              Add Another Subscription
+            </Button>
+          }
         </Panel>
         {this.state.invalid &&
         <Alert bsStyle="danger" >
           <strong>Missing Required Data!</strong> Make sure the URL and Email are filled out.
+        </Alert>}
+        {this.state.isFailure &&
+        <Alert bsStyle="danger" >
+          <strong>Could not Save Subscription.</strong> {this.state.failureMessage}
+        </Alert>}
+        {this.state.isSubbed &&
+        <Alert bsStyle="success" >
+          <strong>Subscription Saved.</strong> {this.state.successMessage}
         </Alert>}
       </div>
     );
