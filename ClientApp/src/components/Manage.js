f16e0bb70eca5893caf470ed5b063414cf2242ae
Angel Barranco <angelusualle@gmail.com>
2018-11-27 12:35:09 -0500

    Finished authentication switching

--- ClientApp/src/components/Manage.js
+++ ClientApp/src/components/Manage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { FormGroup, ControlLabel, FormControl, HelpBlock, Button, Alert } from 'react-bootstrap';
-import validator from "validator"
+import { FormGroup, ControlLabel, FormControl, HelpBlock, Button, Alert, Table, Form, Glyphicon} from 'react-bootstrap';
+import validator from "validator";
 
 export class Manage extends Component {
   displayName = Manage.name
@@ -13,12 +13,27 @@ export class Manage extends Component {
       authenticated = true;
       email= localStorage.getItem("Skraper3Email");
     }
-    this.state = { subscriptions: [], loading: true, emailSub: email, authenticated:authenticated, invalid:true, isFailure:false};
+    this.state = { subscriptions: [], loading: false, emailSub: email, authenticated:authenticated, invalid:true, isFailure:false, loadingSubs:true};
   }
 
   componentDidMount() {
-    if (this.state.emailSub !== ''){
-      
+    let email = '';
+    if (localStorage.getItem("Skraper3Email") !== null){
+      email= localStorage.getItem("Skraper3Email");
+      this.setState({loadingSubs:true});
+      fetch(`api/Subscription/GetSubs?email=${encodeURIComponent(email)}`).then((response) => {
+        if (response.ok) return response.json().then((t) => {
+          this.setState({authenticated:true, loading:false, loadingSubs:false, subscriptions: t})
+        });
+        else response.text()
+          .then(text => {throw Error(text)})
+          .catch((e) => {
+            this.setState({failureMessage: e.message, isFailure: true, loadingSubs:false, loading: false, authenticated:false});
+          })
+      })
+      .catch((e) => {
+        this.setState({failureMessage: e.message, isFailure: true, loadingSubs:false, loading: false, authenticated:false});
+      });
     }
   }
 
@@ -39,40 +54,28 @@ export class Manage extends Component {
     return 'error'
   }
 
-  checkSubscriptions(){
+  logout(e){
+    localStorage.removeItem("Skraper3Email");
+    this.setState({ subscriptions: [], loading: false, emailSub: '', authenticated:false, invalid:false, isFailure:false, loadingSubs:true})
+  }
+  checkSubscriptions(ex){
+    ex.preventDefault();
+    this.setState({loading:true});
     fetch(`api/Subscription/GetSubs?email=${encodeURIComponent(this.state.emailSub)}`).then((response) => {
-      if (response.ok) return response.json().then((t) => this.setState({authenticated:true, loading:false, subscriptions: t}));
-      else response.text().then(text => {throw Error(text)}).catch((e) => {
-        this.setState({failureMessage: e.message, isFailure: true, loading:false})})
+      if (response.ok) return response.json().then((t) => {
+        this.setState({authenticated:true, loading:false, loadingSubs:false, subscriptions: t})
+        localStorage.setItem("Skraper3Email", this.state.emailSub);
+      });
+      else response.text()
+        .then(text => {throw Error(text)})
+        .catch((e) => {
+          this.setState({failureMessage: e.message, isFailure: true, loadingSubs:false, loading:false});
+        })
     })
     .catch((e) => {
-      this.setState({failureMessage: e.message, isFailure: true, loading: false})}
-    );
-  }
+      this.setState({failureMessage: e.message, isFailure: true, loadingSubs:false, loading: false});
+    });
 
-  static renderForecastsTable(forecasts) {
-    return (
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.dateFormatted}>
-              <td>{forecast.dateFormatted}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
   }
 
   render() {
@@ -80,38 +83,72 @@ export class Manage extends Component {
       <div>
         {this.state.authenticated ? 
         <div>
-          <h1>Your subscriptions:</h1>
-          {this.renderForecastsTable()}
+          <h1>Subscriptions</h1>
+          <p>{this.state.emailSub}</p>
+          {this.state.loadingSubs ? 'loading...' :
+          <div>
+            <Table responsive striped bordered condensed hover>
+              <thead>
+                <tr>
+                  <th>URL</th>
+                  <th>Mobile Number</th>
+                  <th>XPath</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.subscriptions.map(sub =>
+                  <tr key={sub.email + sub.url}>
+                    <td>{sub.url}</td>
+                    <td>{sub.mobileNumber}</td>
+                    <td>{sub.xpath}</td>
+                    <td>x</td>
+                  </tr>
+                )}
+              </tbody>
+            </Table>
+            <Button 
+            bsStyle="primary" 
+            bsSize="large" 
+            onClick={(e) => this.logout(e)}
+            >
+            <Glyphicon glyph='log-out' />   Log out
+            </Button>
+          </div>
+          }
         </div>
         :
         <div>
           <h3>Enter your email to manage your subscriptions:</h3>
-          <FieldGroup
-              id="emailSub"
-              name="Email"
-              type="email"
-              required
-              label="Email"
-              onChange={(e) => this.handleChange(e.target.id, e.target.value)}
-              value={this.state.emailSub}
-              disabled={this.state.isLoading}
-              help="Example: YourEmail@Domain.com"
-              validationState={this.getValidationStateEmail()}
-            />
-            <Button 
-              bsStyle="primary" 
-              bsSize="large" 
-              disabled={this.state.invalid} 
-              onClick={(e) => this.checkSubscriptions(e)}
-            >
-            See your subscriptions
-            </Button>
-            <br></br>
-            <br></br>
-            {this.state.isFailure &&
-          <Alert bsStyle="danger" >
-            {this.state.failureMessage}
-          </Alert>}
+          <Form onSubmit={((e) => this.checkSubscriptions(e))}>
+            <FieldGroup
+                id="emailSub"
+                name="Email"
+                type="email"
+                required
+                label="Email"
+                onChange={(e) => this.handleChange(e.target.id, e.target.value)}
+                value={this.state.emailSub}
+                disabled={this.state.isLoading}
+                help="Example: YourEmail@Domain.com"
+                validationState={this.getValidationStateEmail()}
+              />
+              <Button 
+                bsStyle="primary" 
+                bsSize="large" 
+                type="submit"
+                disabled={this.state.invalid || this.state.loading}
+              >
+              {this.state.loading && 'Loading Subscriptions...'}
+                {!this.state.loading && 'See your subscriptions'}
+              </Button>
+              <br></br>
+              <br></br>
+              {this.state.isFailure &&
+            <Alert bsStyle="danger" >
+              {this.state.failureMessage}
+            </Alert>}
+          </Form>
         </div>
       }
       </div>
